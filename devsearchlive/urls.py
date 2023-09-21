4aec8cd1da175325d3c5971775cd288d2094f665
Muhammad-Adam1 <m.adamgee974@gmail.com>
2023-09-21 20:25:01 +0500

    feat:user-profile

--- devsearchlive/urls.py
+++ devsearchlive/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # + static(settings.STATIC, document_root=settings.MEDIA_ROOT) for the collect static
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+def profiles(request):
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
+
+def userprofile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    context = {'profile': profile, 'topSkills':topSkills, 'otherSkills':otherSkills}
+    return render(request, 'users/user-profile.html', context)
