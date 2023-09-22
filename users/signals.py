ca4ac38ca7c21ef56d82ebb646b17b7e9883e4aa
Muhammad-Adam1 <m.adamgee974@gmail.com>
2023-09-22 15:58:34 +0500

    feat: Signals, Login, Logout

---
+++ users/signals.py
@@ -0,0 +1,24 @@
+from .  models import Profile
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+            )
+
+@receiver(post_delete, sender=Profile)
+def delelteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+
+# post_save.connect(createProfile, sender=User)
+# post_delete.connect(delelteUser, sender=Profile)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .models import Profile
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect("profiles")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Username does not exist")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request,"Username or Password is incorrect")
+    print(request.POST)
+    return render(request, "users/login_register.html")
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request,"User is logged out!")
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
