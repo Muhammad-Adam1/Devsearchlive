bbcdb7284d537fb53e073faff1c9aa3963cdfbe8
Muhammad-Adam1 <m.adamgee974@gmail.com>
2023-10-05 15:42:38 +0500

    feat: users can message each other

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from . models import Profile, Skill
+from . models import Profile, Skill, Message
 
 
 class CustomeUserCreationForm(UserCreationForm):
@@ -35,6 +35,16 @@ class SkillForm(ModelForm):
         exclude = ['owner']
     def __init__(self, *args, **kwargs):
         super(SkillForm, self).__init__(*args, **kwargs)
+
+        for key, value in self.fields.items():
+            value.widget.attrs.update({'class': 'input'})
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'subject', 'body']
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
 
         for key, value in self.fields.items():
             value.widget.attrs.update({'class': 'input'})
--- users/urls.py
+++ users/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path('create-skill/', views.createSkill, name="create-skill"),
     path('update-skill/<str:pk>/', views.updateSkill, name="update-skill"),
     path('delete-skill/<str:pk>/', views.deleteSkill, name="delete-skill"),
+    
+    path('inbox/', views.inbox, name="inbox"),
+    path('inbox/<str:pk>/', views.viewMessage, name="message"),
+    path('create-message/<str:pk>/', views.createMessage, name="create-message"),
+
 ]
