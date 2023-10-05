6ec376f155e91edb08fd2b0c731310a502fbf22e
Muhammad-Adam1 <m.adamgee974@gmail.com>
2023-10-05 18:39:52 +0500

    feat: Password reset through email

--- devsearchlive/urls.py
+++ devsearchlive/urls.py
@@ -18,10 +18,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# for email reset 
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
+
+    # in this page we write our email to reset the password 
+    # here we tell to render which template 
+    path('reset_password/', auth_view.PasswordResetView.as_view(template_name="reset_password.html"),
+        name="reset_password"),
+    # this link tell that the reset link password is sent 
+    path('reset_password_sent/', auth_view.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"),
+        name="password_reset_done"),
+    # uidb64 = user id with the base 64 encryption 
+    # in this page we can reset our password by writing down in the fields 
+    path('reset/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(template_name="reset.html"),
+        name="password_reset_confirm"),
+    # this page tell that the password is successfully changed or not 
+    path('reset_password_complete/', auth_view.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"), 
+        name="password_reset_complete"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # + static(settings.STATIC, document_root=settings.MEDIA_ROOT) for the collect static
